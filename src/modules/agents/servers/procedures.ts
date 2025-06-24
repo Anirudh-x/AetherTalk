8060c78569288e00ba4d02b77f12dd26656e5e62
Aniruddha Raut <aniruddharautofficial@gmail.com>
2025-06-24 21:16:22 +0530

    Added Data Table and Empty State Components

--- src/modules/agents/servers/procedures.ts
+++ src/modules/agents/servers/procedures.ts
@@ -4,13 +4,15 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 
 export const agentsRouter = createTRPCRouter({
 
-  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({input}) => {
-    const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
+  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+    const [existingAgent] = await db.select({ meetingCount: sql<number> `5`, ...getTableColumns(agents), })
+                                    .from(agents)
+                                    .where(eq(agents.id, input.id));
 
     return existingAgent;
   }),
@@ -23,7 +25,7 @@ export const agentsRouter = createTRPCRouter({
 
   create: protectedProcedure.input(agentsInsertSchema).mutation(
     async ({ input, ctx }) => {
-      const [createdAgent] = await db.insert(agents).values({...input, userId: ctx.auth.user.id}).returning();
+      const [createdAgent] = await db.insert(agents).values({ ...input, userId: ctx.auth.user.id }).returning();
 
       return createdAgent;
     }
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,6 +5,9 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 
 export const AgentsView = () => {
@@ -12,9 +15,13 @@ export const AgentsView = () => {
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <div>
-      
-      {JSON.stringify(data, null, 2)}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {
+        data.length === 0 && (
+          <EmptyState title="Create your first agent" description="Create your personal agent to join your meetings, attend your calls and interact with yourself." />
+        )
+      }
     </div>
   )
 }
