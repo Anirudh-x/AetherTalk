d04b88c80e00aa258e7c06fcb32db222d338e21d
Aniruddha Raut <aniruddharautofficial@gmail.com>
2025-06-25 20:19:47 +0530

    Added agent update and remove procedure. Added UI for Edit and Delete

--- src/modules/agents/ui/components/agents-form.tsx
+++ src/modules/agents/ui/components/agents-form.tsx
@@ -36,6 +36,22 @@ export const AgentForm =({
           trpc.agents.getMany.queryOptions({}),
         );
 
+
+        onSuccess?.();
+      },
+      onError: (error) => {
+        toast.error(error.message)
+      },
+    })
+  )
+
+  const updateAgent = useMutation(
+    trpc.agents.update.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.agents.getMany.queryOptions({}),
+        );
+
         if (initialValues?.id) {
           await queryClient.invalidateQueries(
             trpc.agents.getOne.queryOptions({ id: initialValues.id })
@@ -59,16 +75,15 @@ export const AgentForm =({
   })
 
   const isEdit = !!initialValues?.id;
-  const isPending = createAgent.isPending;
+  const isPending = createAgent.isPending || updateAgent.isPending;
 
   const onSubmit = (values: z.infer<typeof agentsInsertSchema>) => {
     if (isEdit) {
-      console.log("TODO: UpdateAgent");
+      updateAgent.mutate({...values, id: initialValues.id})
     } else {
       createAgent.mutate(values);
     }
   }
-
 
   return (
     <Form {...form}>
