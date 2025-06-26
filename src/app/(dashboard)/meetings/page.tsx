68892960c383d57e449397b056e21fba2de390ac
Aniruddha Raut <aniruddharautofficial@gmail.com>
2025-06-26 10:36:06 +0530

    Added Meetings schema, module, procedures and pages

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,8 +1,8 @@
 import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from "@/modules/meetings/ui/views/meetings-view"
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 
 
